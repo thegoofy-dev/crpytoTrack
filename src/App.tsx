a5186ded4c5a2831f91ead036407ddccaf7eda59
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-05-07 21:29:22 +0530

    Created Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar"
+
 
 const App = () => {
   return (
-    <div>
-      Crypto Track web App
+    <div className="app">
+      <Navbar />
     </div>
   )
 }
