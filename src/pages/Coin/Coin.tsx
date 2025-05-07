9f7a900910bddd76218765438f4bff578d8ba1c0
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-05-07 22:04:47 +0530

    Created Home Page

---
+++ src/pages/Coin/Coin.tsx
@@ -0,0 +1,11 @@
+import "./Coin.css";
+
+const Coin = () => {
+  return (
+    <div>
+      Coin Page
+    </div>
+  )
+}
+
+export default Coin
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,27 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="hero">
+        <h1>Largest <br />Crypto MarketPlace</h1>
+        <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+        <form>
+          <input type="text" placeholder="Search Crypto.." />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign:"center"}}>24H Change</p>
+          <p className="market-cap">Market Cap</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
